5c4cec51ac6820b836db2977f5f76138a47b46a2
Keernom <keernom.gdev@gmail.com>
2024-02-16 21:33:41 +0300

    feat: AuthOptions with jwt-const's added

---
+++ ASP.NET_React_app/Models/AuthOptions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace ASP.NET_React_app.Models
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "MyAuthServer";
+        public const string AUDIENCE = "MyAuthClient";
+        const string KEY = "my_super_secret_key_123";
+        public const int LIFETIME = 10;
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
