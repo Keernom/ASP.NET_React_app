abb3590625767476da7d4ad21fe84f8106b3db36
Keernom <keernom.gdev@gmail.com>
2024-02-16 20:09:02 +0300

    Add project files.

---
+++ ASP.NET_React_app/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_React_app.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAccount() 
+        { 
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public IActionResult CreateAccount() 
+        { 
+            throw new NotImplementedException(); 
+        }
+
+        [HttpPost]
+        public IActionResult GetToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPatch]
+        public IActionResult UpdateAccount()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteAccount(int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
