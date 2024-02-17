ba602555d68685a5c14a3b099883b6acadf95d86
Keernom <keernom.gdev@gmail.com>
2024-02-17 14:53:26 +0300

    feat: randomly generated key added

--- ASP.NET_React_app/Models/AuthOptions.cs
+++ ASP.NET_React_app/Models/AuthOptions.cs
@@ -7,7 +7,7 @@ namespace ASP.NET_React_app.Models
     {
         public const string ISSUER = "MyAuthServer";
         public const string AUDIENCE = "MyAuthClient";
-        const string KEY = "my_super_secret_key_123";
+        const string KEY = "j4hR6QddW/7a2fK6XZ55bHapSRlrMnklu+nn0m4isJA=";
         public const int LIFETIME = 10;
         public static SymmetricSecurityKey GetSymmetricSecurityKey()
         {
