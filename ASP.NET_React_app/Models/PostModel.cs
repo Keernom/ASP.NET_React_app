35064ff88938f86e3c23716f7f12e34b84be26d9
Keernom <keernom.gdev@gmail.com>
2024-02-18 14:09:00 +0300

    feat: postModel added

---
+++ ASP.NET_React_app/Models/PostModel.cs
@@ -0,0 +1,19 @@
+﻿namespace ASP.NET_React_app.Models
+{
+    public class PostModel
+    {
+        public int Id { get; set; }
+        public string Text { get; private set; }
+        public byte[] Image { get; private set; }
+        public int LikesCount { get; set; }
+        public DateTime PostDate { get; set; }
+
+        public PostModel(int id, string text, byte[] image, DateTime date)
+        {
+            Id = id;
+            Text = text;
+            Image = image;
+            PostDate = date;
+        }
+    }
+}
