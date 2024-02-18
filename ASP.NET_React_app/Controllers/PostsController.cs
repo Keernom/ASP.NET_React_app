67d7117acfb4180203f4824edcba534882d903e2
Keernom <keernom.gdev@gmail.com>
2024-02-18 14:09:28 +0300

    feat: post controller added

---
+++ ASP.NET_React_app/Controllers/PostsController.cs
@@ -0,0 +1,34 @@
+﻿using ASP.NET_React_app.Data.Entities;
+using ASP.NET_React_app.Models;
+using ASP.NET_React_app.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_React_app.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class PostsController : Controller
+    {
+        private PostsService _postService;
+
+        public PostsController(PostsService postService)
+        {
+            _postService = postService;
+        }
+
+        [HttpGet("{userId}")]
+        public ActionResult<List<Post>> GetByAuthor(int userId)
+        {
+            List<Post> posts = _postService.GetByAuthor(userId);
+            return Ok(posts);
+        }
+
+        [HttpPost] 
+        public IActionResult Create([FromBody] PostModel postModel)
+        {
+
+        }
+    }
+}
