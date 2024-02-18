8f8ecb031f666a35fc7233e50840bb60d4e3e785
Keernom <keernom.gdev@gmail.com>
2024-02-18 17:49:12 +0300

    feat: Likes and UserSubs tables removed

--- ASP.NET_React_app/Data/AppDbContext.cs
+++ ASP.NET_React_app/Data/AppDbContext.cs
@@ -7,8 +7,7 @@ namespace ASP.NET_React_app.Data
     {
         public DbSet<User> Users { get; set; }
         public DbSet<Post> Posts { get; set; }
-        public DbSet<UserSub> UserSubs { get; set; }
-        public DbSet<Like> Likes { get; set; }
+
         public AppDbContext(DbContextOptions<AppDbContext> contextOptions) : base(contextOptions)
         {
             Database.EnsureCreated(); 
