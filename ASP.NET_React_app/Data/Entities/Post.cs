c16fbde92ec4924170b4161ffb9676762210bb14
Keernom <keernom.gdev@gmail.com>
2024-02-18 14:07:59 +0300

    feat: Like, Post, UserSub entities added

---
+++ ASP.NET_React_app/Data/Entities/Post.cs
@@ -0,0 +1,12 @@
+﻿namespace ASP.NET_React_app.Data.Entities
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public byte[] Image { get; set; }
+        public int AuthorId { get; set; }
+        public DateTime PostDate { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ ASP.NET_React_app/Data/Entities/UserSub.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP.NET_React_app.Data.Entities
+{
+    public class UserSub
+    {
+        public int From { get; set; }
+        public int To { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
