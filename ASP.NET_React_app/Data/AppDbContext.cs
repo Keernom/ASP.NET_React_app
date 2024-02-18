3c1d190c05a378060247b57694355d1d63ffdc92
Keernom <keernom.gdev@gmail.com>
2024-02-18 14:08:36 +0300

    feat: Post, UserSubs and Likes dbSet's added

--- ASP.NET_React_app/Data/AppDbContext.cs
+++ ASP.NET_React_app/Data/AppDbContext.cs
@@ -6,11 +6,12 @@ namespace ASP.NET_React_app.Data
     public class AppDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
-
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<UserSub> UserSubs { get; set; }
+        public DbSet<Like> Likes { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> contextOptions) : base(contextOptions)
         {
-            Database.EnsureCreated();
-            
+            Database.EnsureCreated(); 
         }
     }
 }
