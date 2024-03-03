5e807ef7dc039235b02a42489177df7f5771654e
Keernom <keernom.gdev@gmail.com>
2024-03-03 03:53:17 +0300

    feat: styles changed

--- ASP.NET_React_app/ClientApp/src/components/users/UserProfile.js
+++ ASP.NET_React_app/ClientApp/src/components/users/UserProfile.js
@@ -29,13 +29,29 @@ const UserProfile = () => {
     return (
         <div>
             <h2>User Profile</h2>
-            <p>Name: {user.name}</p>
-            <p>Email: {user.email}</p>
-            <p>Description: {user.description}</p>
-            <ImageComponent base64String={user.photo} />
-            <ModalButton modalContent={<UserProfileCreation user={user} setAction={updateUser} />} title={"Редактирование профиля"}></ModalButton>
-            <button type="button" className="btn btn-secondary" onClick={exitFromPorfile}> Exit </button>
-        </div>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+
+                <div className="image-box" style={{ width: '50%' }}>
+                    <ImageComponent base64String={user.photo} />
+                </div>
+
+                <div className="user-data" style={{ margin: '2% 10%' }}>
+                    <p>Name: {user.name}</p>
+                    <p>Email: {user.email}</p>
+                    <p>Description: {user.description}</p>
+
+                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
+                        <ModalButton
+                            modalContent={<UserProfileCreation user={user} setAction={updateUser} />}
+                            title={"Редактирование профиля"}
+                            btnName={"Edit"}></ModalButton>
+                        <button type="button" className="btn btn-secondary" onClick={exitFromPorfile}> Exit </button>
+                    </div>
+
+                </div>
+
+            </div>
+        </div >
     );
 };
 
