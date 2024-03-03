e7a2aec048b3e1dd56d8b03b6272a52eb495d423
Keernom <keernom.gdev@gmail.com>
2024-03-03 21:51:48 +0300

    feat: edit and delete posts added

--- ASP.NET_React_app/ClientApp/src/components/posts/Post.js
+++ ASP.NET_React_app/ClientApp/src/components/posts/Post.js
@@ -1,25 +1,46 @@
 import { useEffect, useState } from "react";
 import ImageComponent from "../ImageComponent";
-import { USERS_URL, sendRequestWithToken } from "../../services/commonService";
-import { getPostsByUserId } from "../../services/postsService";
+import { PROFILE_URL, USERS_URL, sendRequestWithToken } from "../../services/commonService";
+import { deletePost, getPostsByUserId, updatePost } from "../../services/postsService";
+import ModalButton from "../ModalButton";
+import PostCreation from "../posts/PostCreation";
 
 
-export const Post = ({ text, imageStr, date }) => {
+export const Post = ({ id, text, imageStr, date, updateAction }) => {
 
+    const updatePostView = async (post) => {
+        await updatePost(post);
+        updateAction(id);
+    }
+
+    const deletePostView = async () => {
+        await deletePost(id);
+        updateAction(id);
+    }
 
     return (
-        <div>
-            <div className="post-item">
+        <div className="post-item">
+            <div className="post-actions">
+                <ModalButton
+                    modalContent={<PostCreation id={id} oldText={text} oldImage={imageStr} setAction={updatePostView} />}
+                    title={"Edit Post"}
+                    btnName={"Edit Post"} />
+                <button type="button" className="btn btn-danger" onClick={deletePostView}> Delete Post </button>
+            </div>
+            <div className="img-box">
+                <ImageComponent base64String={imageStr} />
+            </div>
+            <div>
                 <p>{date}</p>
                 <p>{text}</p>
             </div>
-            <ImageComponent base64String={imageStr} />
         </div>
     );
 }
 
 export const PostsByUser = ({ userId }) => {
     const [posts, setPosts] = useState([]);
+    const [updateUser, setupdateUser] = useState(0);
 
     const getAllPosts = async () => {
         if (userId === 0) return;
@@ -29,12 +50,12 @@ export const PostsByUser = ({ userId }) => {
 
     useEffect(() => {
         getAllPosts();
-    }, [userId])
+    }, [userId, updateUser])
 
     return (
         <div>
             {posts.map((el, key) => {
-                return <Post key={key} text={el.text} imageStr={el.image} date={el.postDate} />
+                return <Post key={key} id={el.id} text={el.text} imageStr={el.image} date={el.postDate} updateAction={setupdateUser} />
             })}
         </div>
     )
