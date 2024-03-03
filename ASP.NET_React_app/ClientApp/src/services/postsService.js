3e957ccd59007aa94ffdf9a0201d9ad5e280d78e
Keernom <keernom.gdev@gmail.com>
2024-03-03 21:53:54 +0300

    feat: create, update, delete func added

--- ASP.NET_React_app/ClientApp/src/services/postsService.js
+++ ASP.NET_React_app/ClientApp/src/services/postsService.js
@@ -1,7 +1,25 @@
+import { post } from "jquery";
 import { ACCOUNT_URL, LOGIN_URL, POSTS_URL, PROFILE_URL, USERS_URL, sendRequestWithToken } from "./commonService";
 
 
 export async function getPostsByUserId(userId) {
     const allPosts = await sendRequestWithToken(`${POSTS_URL}/${userId}`, 'GET')
     return allPosts;
+}
+
+export async function createPost(post) {
+    post.image = post.image.toString();
+    const res = await sendRequestWithToken(POSTS_URL, 'POST', post)
+    return res;
+}
+
+export async function updatePost(post) {
+    post.image = post.image.toString();
+    const res = await sendRequestWithToken(POSTS_URL, 'PATCH', post)
+    return res;
+}
+
+export async function deletePost(postId) {
+    const res = await sendRequestWithToken(`${POSTS_URL}/${postId}`, 'DELETE')
+    return res;
 }
